1a051c28354f00fb2ff0ec276878d62707ce9ac8
Rustam Kagarmanov <kagarmanov03@list.ru>
2024-09-26 11:58:15 +0300

    Обновление сайта

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ЛЮБЛЮ КАТЮ!</Text>
+      <Text>HLTV</Text>
       <StatusBar style="auto" />
     </View>
   );
