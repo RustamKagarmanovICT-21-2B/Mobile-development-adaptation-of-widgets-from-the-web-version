e1ad9e09efa138e24f0e645a37c243e2a6b8d4d0
Rustam Kagarmanov <kagarmanov03@list.ru>
2024-09-30 12:56:50 +0300

    Новый код для сайта

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
+import moment from 'moment';
 
-export default function App() {
+export default function HLTVApp() {
+  const [matches, setMatches] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // Функция для получения данных о матчах с API
+  const fetchMatches = async () => {
+    try {
+      const response = await fetch('https://hltv-api.vercel.app/api/matches.json');
+      const data = await response.json();
+      setMatches(data);
+      setLoading(false);  // Остановить индикатор загрузки
+    } catch (error) {
+      console.error('Ошибка при получении данных:', error);
+      setLoading(false);
+    }
+  };
+
+  // Загрузка данных при первом рендеринге
+  useEffect(() => {
+    fetchMatches();
+  }, []);
+
+  // Если данные еще загружаются, показываем индикатор загрузки
+  if (loading) {
+    return (
+      <View style={styles.loaderContainer}>
+        <ActivityIndicator size="large" color="#00ff00" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>HLTV</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Предстоящие матчи CS:GO</Text>
+      <FlatList
+  data={matches}
+  keyExtractor={(item) => item.id.toString()}
+  renderItem={({ item }) => (
+    <View style={styles.match}>
+      <Text style={styles.matchText}>{item.teams[0].name} vs {item.teams[1].name}</Text>
+      <Text style={styles.dateText}>{moment(item.date).format('MMMM Do YYYY, h:mm:ss a')}</Text>
+    </View>
+  )}
+
+      />
     </View>
   );
 }
@@ -13,8 +54,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    paddingTop: 40,
+    backgroundColor: '#282C34',
+    paddingHorizontal: 10,
+  },
+  loaderContainer: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  match: {
+    padding: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  matchText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  dateText: {
+    fontSize: 14,
+    color: '#bbb',
   },
 });
